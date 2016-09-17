e959f47bb9b42ef7b7c7d1336f41bfec900c7287
Jongyun Jung <jongyun.jung@gmail.com>
2016-09-17 14:37:37 +0900

    source update

--- Graph/src/DFS.java
+++ Graph/src/DFS.java
@@ -1,3 +1,4 @@
+import java.io.FileInputStream;
 import java.util.Scanner;
 public class DFS {
 	static int[][] G = new int[100][100];
@@ -15,22 +16,51 @@ public class DFS {
 	}
 	public static void solve()
 	{
-		Scanner sc = new Scanner(System.in);
-		
+		Scanner	sc = new Scanner(System.in);		
+			
 		V = sc.nextInt();
 		E = sc.nextInt();
-		
+
 		int from, to;
 		for(int i = 0; i < E; i++)
 		{
 			from = sc.nextInt();
 			to = sc.nextInt();
-			
+
 			G[from][to] = 1;
 			G[to][from] = 1;
 		}
 		dfs(1);
+		sc.close();		
+	}
+	public static void solve(String file)
+	{
+		Scanner sc; FileInputStream fin;
 		
-		sc.close();
+		try{
+			if(file == null) sc = new Scanner(System.in);
+			else{
+				fin = new FileInputStream(file);
+				sc = new Scanner(fin);
+			}
+			
+			V = sc.nextInt();
+			E = sc.nextInt();
+			
+			int from, to;
+			for(int i = 0; i < E; i++)
+			{
+				from = sc.nextInt();
+				to = sc.nextInt();
+				
+				G[from][to] = 1;
+				G[to][from] = 1;
+			}
+			dfs(1);
+			sc.close();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
--- Graph/src/GraphExam.java
+++ Graph/src/GraphExam.java
@@ -3,7 +3,7 @@ public class GraphExam {
 
 	public static void main(String[] args) {
 
-		//DFS.solve();
+		DFS.solve("dfs_input.txt");
 		//BFS.solve();
 	}
 
