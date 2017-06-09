97c021f932de862efab02e8a7277b367ed7cbd3f
Jongyun Jung <jongyun.jung@gmail.com>
2017-06-09 22:09:19 +0900

    17년 6월 9일

--- String/src/KMP.java
+++ String/src/KMP.java
@@ -1,26 +1,22 @@
-// 찾기 https://www.acmicpc.net/problem/1786
-// kmp 구현해서 문제 풀기
-// T와 P의 길이는 N, M은 1 에서 1000000
-
-import java.util.*;
+import java.util.Scanner;
 	
 public class KMP 
 {
-	static int[] next = new int[1000010];
-	static int[] pos =  new int[1000010];
+	static int[] next;
+	
 	static String t;
 	static String p;
 	
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		 
-		t = sc.nextLine();
-		p = sc.nextLine();
+		t = sc.nextLine(); 
+		p = sc.nextLine(); 
+		next = new int[p.length() + 1];
 		
 		int n = t.length(); int m = p.length();
-		t = t + "\0\0";
-		p = p + "\0\0";
+		t = t + "\0";
+		p = p + "\0";
 		next[0]= -1;
 		int i = 0, j = -1;
 		
@@ -32,8 +28,7 @@ public class KMP
 			next[i] = j;
 		}
 		
-		i = 0; j = 0;
-		int cnt = 0;
+		i = 0; j = 0;		
 		while(i < n)
 		{
 			while(j >= 0 && p.charAt(j) != t.charAt(i))
@@ -41,16 +36,10 @@ public class KMP
 			i++; j++;
 			if(j == m)
 			{
-				pos[cnt++] = i - j;
-				//j = 0;
+				System.out.println(t.substring(i - j));
+				break;
 			}
 		}
-		System.out.println(cnt);
-		
-		for(int k = 0; k < cnt; k++)
-			System.out.printf("%d ", pos[k] + 1);
-		
-		System.out.printf("\n");
 		sc.close();
 	}
 }
